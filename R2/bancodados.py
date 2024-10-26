b5ac75ea54a69f058032cf26106a9620d44d5f50
Augusto Rubio da Camara <augustorubio2@gmail.com>
2024-10-26 16:58:14 -0300

    Adicionado ao arquivo de banco de dados a estrutura para o armazenamento dos PCs.

--- R2/bancodados.py
+++ R2/bancodados.py
@@ -1,5 +1,6 @@
 import sqlite3
 import os
+from datetime import datetime
 
 db_file = 'db.sqlite3'
 
@@ -18,7 +19,7 @@ def check_db_integrity():
 def create_db():
     with sqlite3.connect(db_file) as conn:
         cursor = conn.cursor()
-        # Create your tables here
+        # Cria a tabela usuarios
         cursor.execute('''
         CREATE TABLE usuarios (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -28,14 +29,50 @@ def create_db():
             senha TEXT NOT NULL
         )
         ''')
-        # Insert initial admin user
+        # Insere o usuário admin inicial
         cursor.execute('''
         INSERT INTO usuarios (usuario, nome_completo, email, senha)
         VALUES ('admin', 'Administrador', 'admin@example.com', 'admin')
         ''')
+        
+        # Cria a tabela computadores
+        cursor.execute('''
+        CREATE TABLE computadores (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            ip TEXT NOT NULL,
+            porta INTEGER NOT NULL,
+            opcoes TEXT,
+            usuario TEXT NOT NULL,
+            senha TEXT NOT NULL
+        )
+        ''')
+        
+        # Cria a tabela alteracoes
+        cursor.execute('''
+        CREATE TABLE alteracoes (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            tabela TEXT NOT NULL,
+            coluna TEXT NOT NULL,
+            valor_antigo TEXT,
+            valor_novo TEXT,
+            data_hora TEXT NOT NULL
+        )
+        ''')
+        
         conn.commit()
 
+def registrar_alteracao(tabela, coluna, valor_antigo, valor_novo):
+    with sqlite3.connect(db_file) as conn:
+        cursor = conn.cursor()
+        data_hora = datetime.now().strftime('%d/%m/%Y %H:%M:%S')  # Formato PT-BR
+        cursor.execute('''
+        INSERT INTO alteracoes (tabela, coluna, valor_antigo, valor_novo, data_hora)
+        VALUES (?, ?, ?, ?, ?)
+        ''', (tabela, coluna, valor_antigo, valor_novo, data_hora))
+        conn.commit()
+
+# Verifica se o arquivo do banco de dados existe ou se a integridade do banco de dados está comprometida
 if not os.path.exists(db_file) or not check_db_integrity():
     if os.path.exists(db_file):
-        os.remove(db_file)
-    create_db()
+        os.remove(db_file)  # Remove o arquivo do banco de dados se ele existir
+    create_db()  # Cria um novo banco de dados
